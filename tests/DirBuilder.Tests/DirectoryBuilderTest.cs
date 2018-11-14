b069f7bce557b32bab17e08ed14ddc18ea54cf14
Robson da Silva <me@rsilva.net>
2018-11-13 23:47:15 -0200

    Tests for directory creation

--- tests/DirBuilder.Tests/DirectoryBuilderTest.cs
+++ tests/DirBuilder.Tests/DirectoryBuilderTest.cs
@@ -10,39 +10,78 @@ namespace DirBuilder.Tests
         [Fact]
         public void Can_Describe()
         {
-            var tempPath = Path.GetTempPath().TrimEnd('/');
+            var temp = Path.GetTempPath();
+            var guid = Guid.NewGuid().ToString();
+            var tempPath = Path.Combine(temp, guid).TrimEnd('/');
+            
+            Directory.CreateDirectory(tempPath);
 
-            var builder = new DirectoryBuilder(tempPath)
-                .AddFile("file_1.txt")
-                .AddFile("file_2.txt")
-                .AddSubdirectoryAndEnter("dir_1")
-                    .AddFile("file_1_1.txt")
-                    .AddFile("file_1_2.txt")
+            try
+            {
+                var builder = new DirectoryBuilder(tempPath)
+                    .AddFile("file1.txt")
+                    .AddFile("file2.txt")
+                    .AddSubdirectoryAndEnter("dir1")
+                        .AddFile("file11.txt")
+                        .AddFile("file12.txt")
                     .GoBack()
-                .AddSubdirectoryAndStay("dir_2")
-                .AddSubdirectoryAndEnter("dir_3")
-                    .AddSubdirectoryAndEnter("dir_3_1")
-                        .AddFile("file_3_1_1.txt")
-                        .AddFile("file_3_1_2.txt")
+                    .AddSubdirectoryAndStay("dir2")
+                    .AddSubdirectoryAndEnter("dir3")
+                        .AddSubdirectoryAndEnter("dir31")
+                            .AddFile("file311.txt")
+                            .AddFile("file312.txt")
                         .GoBack()
-                    .AddFile("file_3.1.txt")
+                        .AddFile("file31.txt")
                     .GoBack();
 
-            var expected = 
-                tempPath + "\r\n" + 
-                tempPath + @"/file_1.txt" + "\r\n" + 
-                tempPath + @"/file_2.txt" + "\r\n" + 
-                tempPath + @"/dir_1" + "\r\n" + 
-                tempPath + @"/dir_1/file_1_1.txt" + "\r\n" + 
-                tempPath + @"/dir_1/file_1_2.txt" + "\r\n" + 
-                tempPath + @"/dir_2" + "\r\n" + 
-                tempPath + @"/dir_3" + "\r\n" + 
-                tempPath + @"/dir_3/file_3.1.txt" + "\r\n" + 
-                tempPath + @"/dir_3/dir_3_1" + "\r\n" + 
-                tempPath + @"/dir_3/dir_3_1/file_3_1_1.txt" + "\r\n" + 
-                tempPath + @"/dir_3/dir_3_1/file_3_1_2.txt" + "\r\n"; 
+                var expected = 
+                    tempPath + "\r\n" + 
+                    tempPath + @"/file1.txt" + "\r\n" + 
+                    tempPath + @"/file2.txt" + "\r\n" + 
+                    tempPath + @"/dir1" + "\r\n" + 
+                    tempPath + @"/dir1/file11.txt" + "\r\n" + 
+                    tempPath + @"/dir1/file12.txt" + "\r\n" + 
+                    tempPath + @"/dir2" + "\r\n" + 
+                    tempPath + @"/dir3" + "\r\n" + 
+                    tempPath + @"/dir3/file31.txt" + "\r\n" + 
+                    tempPath + @"/dir3/dir31" + "\r\n" + 
+                    tempPath + @"/dir3/dir31/file311.txt" + "\r\n" + 
+                    tempPath + @"/dir3/dir31/file312.txt" + "\r\n"; 
 
-            Assert.Equal(expected, builder.ToString());
+                Assert.Equal(expected, builder.ToString());
+            }
+            finally
+            {
+                Directory.Delete(tempPath, true);
+            }
+        }
+    
+        [Fact]
+        public void Can_Create()
+        {
+            var temp = Path.GetTempPath();
+            var guid = Guid.NewGuid().ToString();
+            var tempPath = Path.Combine(temp, guid).TrimEnd('/');
+            
+            Directory.CreateDirectory(tempPath);
+
+            try
+            {
+                var builder = new DirectoryBuilder(tempPath)
+                    .AddFile("file1.txt")
+                    .AddSubdirectoryAndEnter("dir1")
+                        .AddFile("file11.txt");
+
+                builder.Create();
+
+                Assert.True(File.Exists(Path.Combine(tempPath, "file1.txt")));
+                Assert.True(Directory.Exists(Path.Combine(tempPath, "dir1")));
+                Assert.True(File.Exists(Path.Combine(tempPath, "dir1//file11.txt")));
+            }
+            finally
+            {
+                Directory.Delete(tempPath, true);
+            }
         }
     }
 }
