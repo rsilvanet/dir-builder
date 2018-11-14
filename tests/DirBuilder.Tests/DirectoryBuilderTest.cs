14f69000a40b71e06d4a225fc02f525279ac1977
Robson da Silva <me@rsilva.net>
2018-11-14 09:13:57 -0200

    More unit tests

--- tests/DirBuilder.Tests/DirectoryBuilderTest.cs
+++ tests/DirBuilder.Tests/DirectoryBuilderTest.cs
@@ -9,7 +9,7 @@ namespace DirBuilder.Tests
     public class DirectoryBuilderTest
     {
         [Fact]
-        public void Can_Describe()
+        public void Can_Create_Base_Dir_If_Dont_Exists()
         {
             var temp = Path.GetTempPath();
             var guid = Guid.NewGuid().ToString();
@@ -17,37 +17,9 @@ namespace DirBuilder.Tests
 
             try
             {
-                var dirBuilder = new DirectoryBuilder(tempPath, true)
-                    .AddFile("file1.txt")
-                    .AddFile("file2.txt")
-                    .AddSubdirectoryAndEnter("dir1")
-                        .AddFile("file11.txt")
-                        .AddFile("file12.txt")
-                    .GoBack()
-                    .AddSubdirectoryAndStay("dir2")
-                    .AddSubdirectoryAndEnter("dir3")
-                        .AddSubdirectoryAndEnter("dir31")
-                            .AddFile("file311.txt")
-                            .AddFile("file312.txt")
-                        .GoBack()
-                    .   AddFile("file31.txt")
-                    .GoBack();
-
-                var strBuilder = new StringBuilder();
-                strBuilder.AppendLine(tempPath);
-                strBuilder.AppendLine(Path.Combine(tempPath, @"file1.txt"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"file2.txt"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir1"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir1/file11.txt"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir1/file12.txt"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir2"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir3"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/file31.txt"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/dir31"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/dir31/file311.txt"));
-                strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/dir31/file312.txt"));
-
-                Assert.Equal(strBuilder.ToString(), dirBuilder.ToString());
+                var dirBuilder = new DirectoryBuilder(tempPath, true);
+                dirBuilder.Create();
+                Directory.Exists(tempPath);
             }
             finally
             {
@@ -56,7 +28,154 @@ namespace DirBuilder.Tests
         }
 
         [Fact]
-        public void Can_Create()
+        public void Throws_If_Base_Dir_Dont_Exists()
+        {
+            var temp = Path.GetTempPath();
+            var guid = Guid.NewGuid().ToString();
+            var tempPath = Path.Combine(temp, guid).TrimEnd('/');
+
+            Assert.Throws<DirectoryNotFoundException>(() =>
+            {
+                new DirectoryBuilder(tempPath, false);
+            });
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Throws_If_Adding_Empty_Dir_Name(string dirName)
+        {
+            var dirBuilder = new DirectoryBuilder(Path.GetTempPath());
+
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddSubdirectoryAndStay(dirName);
+            });
+
+            Assert.Equal("directoryName", exception.ParamName);
+            Assert.Contains("Directory name can't be null or empty", exception.Message);
+
+            exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddSubdirectoryAndEnter(dirName);
+            });
+
+            Assert.Equal("directoryName", exception.ParamName);
+            Assert.Contains("Directory name can't be null or empty", exception.Message);
+        }
+
+        [Theory(Skip = "Method ValidateDirectoryName() need to be changed")]
+        [InlineData("&*#@")]
+        public void Throws_If_Adding_Invalid_Dir_Name(string dirName)
+        {
+            var dirBuilder = new DirectoryBuilder(Path.GetTempPath());
+
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddSubdirectoryAndStay(dirName);
+            });
+
+            Assert.Equal("directoryName", exception.ParamName);
+            Assert.Contains("Directory name can't contains invalid chars", exception.Message);
+
+            exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddSubdirectoryAndEnter(dirName);
+            });
+
+            Assert.Equal("directoryName", exception.ParamName);
+            Assert.Contains("Directory name can't contains invalid chars", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Throws_If_Adding_Empty_File_Name(string fileName)
+        {
+            var dirBuilder = new DirectoryBuilder(Path.GetTempPath());
+
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddFile(fileName);
+            });
+
+            Assert.Equal("fileName", exception.ParamName);
+            Assert.Contains("File name can't be null or empty", exception.Message);
+
+            exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddFile(fileName);
+            });
+
+            Assert.Equal("fileName", exception.ParamName);
+            Assert.Contains("File name can't be null or empty", exception.Message);
+        }
+
+        [Theory(Skip = "Method ValidateFileName() need to be changed")]
+        [InlineData("&*#@")]
+        public void Throws_If_Adding_Invalid_File_Name(string fileName)
+        {
+            var dirBuilder = new DirectoryBuilder(Path.GetTempPath());
+
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddFile(fileName);
+            });
+
+            Assert.Equal("fileName", exception.ParamName);
+            Assert.Contains("File name can't contains invalid chars", exception.Message);
+
+            exception = Assert.Throws<ArgumentException>(() =>
+            {
+                dirBuilder.AddFile(fileName);
+            });
+
+            Assert.Equal("fileName", exception.ParamName);
+            Assert.Contains("File name can't contains invalid chars", exception.Message);
+        }
+
+        [Fact]
+        public void Can_Describe_Correctly()
+        {
+            var tempPath = Path.GetTempPath().TrimEnd('/');
+
+            var dirBuilder = new DirectoryBuilder(tempPath, true)
+                .AddFile("file1.txt")
+                .AddFile("file2.txt")
+                .AddSubdirectoryAndEnter("dir1")
+                    .AddFile("file11.txt")
+                    .AddFile("file12.txt")
+                .GoBack()
+                .AddSubdirectoryAndStay("dir2")
+                .AddSubdirectoryAndEnter("dir3")
+                    .AddSubdirectoryAndEnter("dir31")
+                        .AddFile("file311.txt")
+                        .AddFile("file312.txt")
+                    .GoBack()
+                .AddFile("file31.txt")
+                .GoBack();
+
+            var strBuilder = new StringBuilder();
+            strBuilder.AppendLine(tempPath);
+            strBuilder.AppendLine(Path.Combine(tempPath, @"file1.txt"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"file2.txt"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir1"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir1/file11.txt"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir1/file12.txt"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir2"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir3"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/file31.txt"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/dir31"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/dir31/file311.txt"));
+            strBuilder.AppendLine(Path.Combine(tempPath, @"dir3/dir31/file312.txt"));
+
+            Assert.Equal(strBuilder.ToString(), dirBuilder.ToString());
+        }
+
+        [Fact]
+        public void Can_Create_Correctly()
         {
             var temp = Path.GetTempPath();
             var guid = Guid.NewGuid().ToString();
